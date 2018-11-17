f5e4ca0840a4f088e710416492aa8e1c20c89bc7
Piperiveros <felipe.riveros.f@gmail.com>
2018-11-16 21:10:34 -0300

    add: agrega ejercicio

---
+++ assets/js/lib/data.js
@@ -0,0 +1,36 @@
+$(function(){
+
+	$('.cards').on('click', '.card', function(){
+		$(this).toggleClass('card--open');
+		
+	});
+
+	$('.card').on('click', '.card__like', function(){
+		$(this).toggleClass('card__like--red');
+		event.preventDefault();
+		event.stopPropagation();
+	})
+
+	$('.card__follow-btn').on('click', function(){
+		$(this).toggleClass('card__follow-btn--following');
+		event.preventDefault();
+		event.stopPropagation();
+	})
+
+	$('#image').on('change', function(){
+		var imagen = 'assets/images/squared/' + $(this).val();
+
+		$('.create__image .create__img').attr('src', imagen);
+
+	})
+
+	$('#author').on('change', function(){
+		var imagen = 'assets/images/profiles/' + $(this).val();
+
+		$('.create__profile .create__img').attr('src', imagen);
+
+	})
+
+	$('#followers') 
+
+})
